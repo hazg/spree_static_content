f255cea98eed086c5f397e0c39372ff2059d1515
hazg <hazg@mail.ru>
2011-11-30 15:34:00 +0400

    cleanup code

--- app/controllers/spree/static_content_controller.rb
+++ app/controllers/spree/static_content_controller.rb
@@ -1,10 +1,9 @@
 module Spree
   class StaticContentController < Spree::BaseController
-    #caches_action :show
+    caches_action :show
     
     def show
-      path = params[:path]
-      case params[:path]
+      path = case params[:path]
       when Array
         '/' + params[:path].join("/")
       when String
@@ -12,8 +11,6 @@ module Spree
       when nil
         request.path
       end
-      #path = path.gsub(/^\/*/, '')
-      l = Rails.logger.debug("**********************#{path}")
       unless @page = Page.visible.find_by_slug(path)
         render_404
       end
